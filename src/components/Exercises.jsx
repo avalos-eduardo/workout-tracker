2be25936330e5def8a35b4df9dd54f6315fc56e3
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-04-29 19:55:28 -0500

    modified to properly import Inter font

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,4 +1,5 @@
 import "../styles/Exercises.css";
+import "@fontsource/inter/700";
 import searchIcon from "../assets/search.png";
 import ExerciseList from "./ExerciseList";
 import { useEffect, useState } from "react";

c3601e38bc481d0439e0f508451d08f04f529d04
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-05-12 23:59:04 -0500

    Set up JSX and styling for headers and buttons

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,5 +1,15 @@
 import Heading from "./Heading";
+import "../styles/Start.css";
 
 export default function Start() {
-  return <Heading headingTitle="Start" />;
+  return (
+    <>
+      <Heading headingTitle="Start" />
+      <button className="start-empty">Start Empty Workout</button>
+      <div className="template-header">
+        <Heading headingTitle="Templates" />
+        <button className="add-new-template">Add New</button>
+      </div>
+    </>
+  );
 }
